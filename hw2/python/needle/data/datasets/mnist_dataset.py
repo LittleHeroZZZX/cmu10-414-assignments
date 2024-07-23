a0f2853695966132e77345d63bf3468724e97c3a
zhouxin <zhou.xin2000@outlook.com>
2024-07-23 14:12:39 +0800

    完成Q4

--- hw2/python/needle/data/datasets/mnist_dataset.py
+++ hw2/python/needle/data/datasets/mnist_dataset.py
@@ -2,6 +2,50 @@ from typing import List, Optional
 from ..data_basic import Dataset
 import numpy as np
 
+import gzip
+
+
+def parse_mnist(image_filename, label_filename):
+    """ Read an images and labels file in MNIST format.  See this page:
+    http://yann.lecun.com/exdb/mnist/ for a description of the file format.
+
+    Args:
+        image_filename (str): name of gzipped images file in MNIST format
+        label_filename (str): name of gzipped labels file in MNIST format
+
+    Returns:
+        Tuple (X,y):
+            X (numpy.ndarray[np.float32]): 2D numpy array containing the loaded 
+                data.  The dimensionality of the data should be 
+                (num_examples x input_dim) where 'input_dim' is the full 
+                dimension of the data, e.g., since MNIST images are 28x28, it 
+                will be 784.  Values should be of type np.float32, and the data 
+                should be normalized to have a minimum value of 0.0 and a 
+                maximum value of 1.0 (i.e., scale original values of 0 to 0.0 
+                and 255 to 1.0).
+
+            y (numpy.ndarray[dtype=np.uint8]): 1D numpy array containing the
+                labels of the examples.  Values should be of type np.uint8 and
+                for MNIST will contain the values 0-9.
+    """
+    ### BEGIN YOUR CODE
+    image_file_handle = gzip.open(image_filename, 'rb')
+    label_file_handle = gzip.open(label_filename, 'rb')
+    image_file_handle.read(16)
+    label_file_handle.read(8)
+    image_data = image_file_handle.read()
+    label_data = label_file_handle.read()
+    image_file_handle.close()
+    label_file_handle.close()
+    X = np.frombuffer(image_data, dtype=np.uint8).reshape(-1, 28*28).astype(np.float32)
+    X = X / 255.0
+    y = np.frombuffer(label_data, dtype=np.uint8)
+    return X, y
+    ### END YOUR CODE
+
+
+
+
 class MNISTDataset(Dataset):
     def __init__(
         self,
@@ -10,15 +54,17 @@ class MNISTDataset(Dataset):
         transforms: Optional[List] = None,
     ):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.transforms = transforms
+        self.X, self.y = parse_mnist(image_filename, label_filename)
+        
         ### END YOUR SOLUTION
-
     def __getitem__(self, index) -> object:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        x = self.apply_transforms(self.X[index].reshape(28, 28, -1))
+        return x.reshape(-1, 28*28), self.y[index]
         ### END YOUR SOLUTION
 
     def __len__(self) -> int:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return self.X.shape[0]
         ### END YOUR SOLUTION
