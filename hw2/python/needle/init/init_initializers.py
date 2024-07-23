f719623a5b5213048dcf6e347aecccb83ca89bd1
zhouxin <zhou.xin2000@outlook.com>
2024-07-23 13:03:22 +0800

    修复初始化函数没有转发kwargs

--- hw2/python/needle/init/init_initializers.py
+++ hw2/python/needle/init/init_initializers.py
@@ -5,14 +5,14 @@ from .init_basic import *
 def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
     a = gain * math.sqrt(6.0 / (fan_in + fan_out))
-    return rand(fan_in, fan_out, low=-a, high=a)
+    return rand(fan_in, fan_out, low=-a, high=a, **kwargs)
     ### END YOUR SOLUTION
 
 
 def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
     std = gain*math.sqrt(2.0 / (fan_in+fan_out))
-    return randn(fan_in, fan_out, mean=0, std=std)
+    return randn(fan_in, fan_out, mean=0, std=std, **kwargs)
     ### END YOUR SOLUTION
 
 
@@ -22,7 +22,7 @@ def kaiming_uniform(fan_in, fan_out, nonlinearity="relu", **kwargs):
     if nonlinearity == "relu":
         gain = math.sqrt(2)
     bound = gain * math.sqrt(3/fan_in)
-    return rand(fan_in, fan_out, low=-bound, high=bound)
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
     ### END YOUR SOLUTION
 
 
@@ -32,5 +32,5 @@ def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
     if nonlinearity == "relu":
         gain = math.sqrt(2)
     std = gain / math.sqrt(fan_in)
-    return randn(fan_in, fan_out, mean=0, std=std)
+    return randn(fan_in, fan_out, mean=0, std=std, **kwargs)
     ### END YOUR SOLUTION
