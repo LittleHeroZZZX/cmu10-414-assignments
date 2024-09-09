c970d9ed1e34e512ccca62f716df542851720c54
zhouxin <zhou.xin2000@outlook.com>
2024-09-09 15:03:33 +0800

    finish part 1

--- hw4/python/needle/ops/ops_logarithmic.py
+++ hw4/python/needle/ops/ops_logarithmic.py
@@ -25,16 +25,31 @@ def logsoftmax(a):
 
 class LogSumExp(TensorOp):
     def __init__(self, axes: Optional[tuple] = None):
+        if isinstance(axes, int):
+            axes = (axes,)
         self.axes = axes
 
     def compute(self, Z):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if self.axes is None:
+            self.axes = tuple(range(len(Z.shape)))
+        max_z = Z.max(axis=self.axes, keepdims=True)
+        var = array_api.log(array_api.sum(array_api.exp(Z - max_z.broadcast_to(Z.shape)), axis=self.axes))
+        return var + max_z.reshape(var.shape)
         ### END YOUR SOLUTION
 
     def gradient(self, out_grad, node):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if self.axes is None:
+            self.axes = tuple(range(len(node.inputs[0].shape)))
+        z = node.inputs[0]
+        shape = [1 if i in self.axes else z.shape[i] for i in range(len(z.shape))]
+        gradient = exp(z - node.reshape(shape).broadcast_to(z.shape))
+        return out_grad.reshape(shape).broadcast_to(z.shape)*gradient
+        
+        
+        
+        
         ### END YOUR SOLUTION
 
 
